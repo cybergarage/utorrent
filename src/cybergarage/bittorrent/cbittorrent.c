78d1f8ab369387810821fcb3d05a5e1a79b62dd7
skonno <skonno@201c92ac-3c32-0410-8a4d-ebfbdf81d61d>
2007-08-03 15:31:39 +0000

    git-svn-id: svn://svn.code.sf.net/p/cgbittorrent/code/trunk@33 201c92ac-3c32-0410-8a4d-ebfbdf81d61d

--- src/cybergarage/bittorrent/cbittorrent.c
+++ src/cybergarage/bittorrent/cbittorrent.c
@@ -19,3 +19,32 @@
 
 #include <cybergarage/bittorrent/cbittorrent.h>
 
+#include <cybergarage/bittorrent/cbencoding.h>
+#include <cybergarage/bittorrent/csha1.h>
+
+/****************************************
+* cg_bittorrent_new
+****************************************/
+
+CgBittorrent *cg_bittorrent_new()
+{
+	CgBittorrent *cb;
+
+	cb = (CgBittorrent *)malloc(sizeof(CgBittorrent));
+	if (!cb)
+		return NULL;
+
+	return cb;
+}
+
+/****************************************
+* cg_bittorrent_delete
+****************************************/
+
+void cg_bittorrent_delete(CgBittorrent *cb)
+{
+	if (!cb)
+		return;
+
+	free(cb);
+}
---
+++ src/cybergarage/bittorrent/cbittorrent_http.c
@@ -0,0 +1,17 @@
+/******************************************************************
+*
+*	CyberBittorrent for C
+*
+*	Copyright (C) Satoshi Konno 2007
+*
+*	File: cbittorrent_http.c
+*
+*	Revision:
+*
+*	08/04/07
+*		- first revision
+*
+******************************************************************/
+
+#include <cybergarage/bittorrent/cbittorrent.h>
+
